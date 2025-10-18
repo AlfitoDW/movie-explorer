f3fb33b714eaf9979493ee8422a79c751e674472
AlfitoDW <alfitoarts@gmail.com>
2025-10-18 23:54:29 +0700

    Update Bug Carousel eror console

--- src/components/MovieCarousel.jsx
+++ src/components/MovieCarousel.jsx
@@ -1,32 +1,46 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import MovieCard from "./MovieCard";
 
 
 export default function MovieCarousel({title, movies}) {
     const rowRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false);
+    const [startX, setStartX] = useState(0);
+    const [scrollLeft, setScrollLeft] = useState(0);
 
-    //wheel hadnler
-    const handleWheel = (e) => {
-        const el = rowRef.current;
-        if(!el) return;
+    const startDrag = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX || e.touches?.[0]?.pageX);
+    setScrollLeft(rowRef.current.scrollLeft);
+    };
 
-        if(Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-            //geser
-            el.scrollLeft += e.deltaY
-            // cegah halaman ikut scroll
-            e.preventDefault();
-        }
-    }
+    const stopDrag = () => {
+        setIsDragging(false);
+    };
+
+    const handleDrag = (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        const x = e.pageX || e.touches?.[0]?.pageX;
+        const walk = (x - startX) * 1.5; // kecepatan geser
+        rowRef.current.scrollLeft = scrollLeft - walk;
+    };
 
     return (
         <div className="mb-8">
             <h2 className="text-xl font-semibold mb-3 text-white">{title}</h2>
             <div 
                 ref={rowRef}
-                onWheel={handleWheel}
-                className="flex overflow-x-auto space-x-4 carousel no-scrollbar"
+                className="flex overflow-x-auto space-x-4 no-scrollbar cursor-grab active:cursor-grabbing"
                 role="list"
                 aria-label={`${title} movies`}
+                onMouseDown={startDrag}
+                onMouseLeave={stopDrag}
+                onMouseUp={stopDrag}
+                onMouseMove={handleDrag}
+                onTouchStart = {startDrag}
+                onTouchEnd={stopDrag}
+                onTouchMove={handleDrag}
                 >
                 {movies.map((movie) => (
                     <MovieCard key={movie.id} movie={movie} />
